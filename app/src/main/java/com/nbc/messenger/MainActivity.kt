c38f1db298d5516da021170a040554c1906c4038
Bettor <be0128st@gmail.com>
2024-04-22 22:31:13 +0900

    1

--- app/src/main/java/com/nbc/messenger/MainActivity.kt
+++ app/src/main/java/com/nbc/messenger/MainActivity.kt
@@ -1,20 +1,27 @@
 package com.nbc.messenger
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.text.TextUtils.replace
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.nbc.messenger.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(binding.root)
+
+
+
+        setFragment(MyPageFragment())
+    }
+    private fun setFragment(frag : Fragment) {
+        supportFragmentManager.commit {
+            replace(R.id.framelayout, frag)
+            setReorderingAllowed(true)
+            addToBackStack("")
         }
     }
 }
