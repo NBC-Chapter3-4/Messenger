516368a0f0f533c87668befbb7165dcf46db2b63
agvber <agvber12@gmail.com>
2024-04-22 19:42:16 +0900

    chore: circleimageview 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.circleimageview)
 
     // TEST
     testImplementation(libs.junit)
